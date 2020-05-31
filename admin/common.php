42c10f5f447575b7a08f3e2296cfb1606bfc3949
qufengwu <38528165+qufengwu@users.noreply.github.com>
2020-05-31 21:10:44 +0800

    first release

---
+++ admin/common.php
@@ -0,0 +1,28 @@
+<?php
+if (!defined('__DIR__')) {
+    define('__DIR__', dirname(__FILE__));
+}
+
+define('__TYPECHO_ADMIN__', true);
+
+/** 载入配置文件 */
+if (!defined('__TYPECHO_ROOT_DIR__') && !@include_once __DIR__ . '/../config.inc.php') {
+    file_exists(__DIR__ . '/../install.php') ? header('Location: ../install.php') : print('Missing Config File');
+    exit;
+}
+
+/** 初始化组件 */
+Typecho_Widget::widget('Widget_Init');
+
+/** 注册一个初始化插件 */
+Typecho_Plugin::factory('admin/common.php')->begin();
+
+Typecho_Widget::widget('Widget_Options')->to($options);
+Typecho_Widget::widget('Widget_User')->to($user);
+Typecho_Widget::widget('Widget_Security')->to($security);
+Typecho_Widget::widget('Widget_Menu')->to($menu);
+
+/** 初始化上下文 */
+$request = $options->request;
+$response = $options->response;
+
